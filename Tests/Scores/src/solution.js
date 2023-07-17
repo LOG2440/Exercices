773cd6c62738ecf0110f08adce8ec18d686e1fe7
Nikolay Radoev <nikolayradoev@gmail.com>
2023-07-17 17:56:50 -0400

    Added README and removed npm start script

--- Tests/Scores/src/solution.js
+++ Tests/Scores/src/solution.js
@@ -34,3 +34,23 @@ export default class ScoreCalculatorSolution {
         return array.reduce((acc, x) => acc + x, 0) / array.length;
     }
 }
+
+
+/*
+Exercice supplémentaire :
+
+Le site web permet de rentrer une note de 0 à 5 (min=0), or la fonction validateStars()
+considère une note valide si elle est entre 1 et 5. Ceci peut causer des problèmes si un
+produit a plusieurs notes de 0 puisque la moyenne calculée ne sera pas la même que celle
+attendue. Un utilisateur peut penser que le système "gonfle" les notes des produits en
+ignorant les notes de 0.
+
+Ceci aurait pu être détecté par des tests d'intégration. Par exemple :
+    [ { author: null, stars: 4 }, { author: "Jean", stars: 5 }, { author: null, stars: 0 }]
+devrait donner une moyenen globale de 3 (4+5+0)/3, mais le résultat sera plutôt 4.5 (4+5)/2 
+
+La validation par le serveur reste une bonne pratique puisqu'on ne voudrait pas que la page
+web soit l'autorité finale sur la note maximale possible, surtout si un utilisateur peut modifier
+la limité sur l'élément HTML.
+
+*/
