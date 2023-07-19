96f3c50ab6cac2168c37781e8b7e27df359ae968
Nikolay Radoev <nikolayradoev@gmail.com>
2023-07-19 14:10:30 -0400

    Backend is in english and now manages course credit assignment

--- Async/Class/server/main.js
+++ Async/Class/server/main.js
@@ -7,7 +7,7 @@ app.use(cors());
 const urlencodedParser = express.urlencoded({ extended: false });
 app.use(express.json());
 
-let listeDeCours = [
+let courses = [
     { sigle: "INF2990", credits: 4 },
     { sigle: "LOG4420", credits: 3 },
     { sigle: "LOG1000", credits: 3 },
@@ -15,44 +15,44 @@ let listeDeCours = [
 ];
 
 app.get("/obtenirCours", function (req, res) {
-    res.status(listeDeCours.length ? 200 : 404).send(listeDeCours);
+    res.status(courses.length ? 200 : 404).send(courses);
 });
 
 app.get("/obtenirCours/:sigle", function (req, res) {
-    const cours = listeDeCours.find((c) => {
+    const course = courses.find((c) => {
         return c.sigle === req.params.sigle;
     });
-    if (!cours) {
+    if (!course) {
         res.status(404).send({ error: "Cours non trouvé" });
         return;
     }
-    res.send(cours);
+    res.send(course);
 });
 
 app.post("/ajouterCours", urlencodedParser, function (req, res) {
     if (!req.body) return res.sendStatus(400);
-    const cours = { sigle: req.body.sigle, credits: parseInt(req.body.credits) };
-    if (listeDeCours.find(x => x.sigle === cours.sigle)) {
-        res.status(409).send({ error: "Cours déjà existant" });
+    const course = { sigle: req.body.sigle, credits: Math.floor(Math.random() * 5 + 1) };
+    if (courses.find(x => x.sigle === course.sigle)) {
+        res.status(409).send({ error: `Cours ${course.sigle} déjà existant` });
         return;
     }
-    listeDeCours.push(cours);
-    res.status(201).send(cours.sigle + " a été ajouté");
+    courses.push(course);
+    res.status(201).send(course.sigle + " a été ajouté");
 });
 
 app.delete("/supprimerCours/:sigle", function (req, res) {
-    const taille = listeDeCours.length;
-    listeDeCours = listeDeCours.filter((c) => c.sigle !== req.params.sigle);
-    if (taille > listeDeCours.length) res.send("Cours supprimé.");
+    const size = courses.length;
+    courses = courses.filter((c) => c.sigle !== req.params.sigle);
+    if (size > courses.length) res.send("Cours supprimé.");
     else res.status(400).send("Echec de suppression : cours introuvable dans la liste");
 });
 
 app.patch("/modifierCours/", urlencodedParser, function (req, res) {
-    const cours = listeDeCours.find((c) => {
+    const course = courses.find((c) => {
         return c.sigle === req.body.sigle;
     });
-    if (!cours) return res.status(404).send("Ce cours n'existe pas");
-    cours.credits = req.body.credits;
+    if (!course) return res.status(404).send("Ce cours n'existe pas");
+    course.credits = req.body.credits;
     res.send("Cours modifié");
 });
 
