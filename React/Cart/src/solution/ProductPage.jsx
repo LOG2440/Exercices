276234a8a400e6f4666afafd73aafd0c8c688be7
Nikolay Radoev <nikolay.radoev@polymtl.ca>
2023-08-15 12:12:55 -0400

    Load products on component creation with useEffect

--- React/Cart/src/solution/ProductPage.jsx
+++ React/Cart/src/solution/ProductPage.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Product from "./Product";
 import PRODUCTS from "../products";
 
 const ProductPage = () => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        // Simuler un chargement des items
+        const fetchProducts = () => PRODUCTS;
+
+        
+        setProducts(fetchProducts());
+    }, []);
     return (
         <>
             <h1 className="center-text">Produits disponibles</h1>
