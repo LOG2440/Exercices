32493c35f8a5c02925e8911e049089897e1fa9a5
Nikolay Radoev <nikolay.radoev@polymtl.ca>
2025-05-28 13:45:20 -0400

    First draft

---
+++ Async/API/solution.js
@@ -0,0 +1,46 @@
+let currentPage = 1;
+
+async function fetchGames(pageIndex = 1) {
+    const res = await fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1&pageSize=20&pageNumber=${pageIndex}`);
+    const games = await res.json();
+
+    const parent = document.getElementById('parent');
+    parent.innerHTML = '';
+    games.forEach(game => {
+        const div = document.createElement('div');
+        div.className = 'game';
+        div.innerHTML = `
+        <h2>${game.title}</h2>
+        <p>Price: $${game.salePrice}</p>
+        <p>Normal Price: $${game.normalPrice}</p>
+        <p>Meta Score: ${game.metacriticScore}</p>
+        <a href="https://www.cheapshark.com/redirect?dealID=${game.dealID}" target="_blank">
+            <img src="${game.thumb}" alt="${game.title}" />
+        </a>
+    `;
+        parent.appendChild(div);
+    });
+
+}
+
+const loadGamesButton = document.getElementById('loadButton');
+loadGamesButton.addEventListener('click', () => {
+    currentPage = 1;
+    fetchGames(currentPage);
+});
+const nextPageButton = document.getElementById('nextPage');
+document.getElementById('nextPage').addEventListener('click', () => {
+    currentPage++;
+    fetchGames(currentPage);
+    prevPageButton.disabled = false;
+}
+);
+
+const prevPageButton = document.getElementById('prevPage');
+document.getElementById('prevPage').addEventListener('click', () => {
+    if (currentPage >= 1) {
+        currentPage--;
+        prevPageButton.disabled = currentPage === 1;
+        fetchGames(currentPage);
+    }
+});
